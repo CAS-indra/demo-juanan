76d51c246e0202cef5c7c75f5c33b2978447d8ce
juanan83 <juanangelpueyo@outlook.com>
2021-09-22 01:50:39 +0200

    Agregados datos delegados a home.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,13 +3,34 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  public proyectos = [
+    {
+      name: 'Aprender angular',
+      status: 1,
+    },
+    {
+      name: 'Tener un buen trabajo',
+      status: 0,
+    },
+  ];
+  public color = 'brown';
+  public elValor = 5;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  public multiplo(cantidad: number): number {
+    return cantidad * this.elValor;
   }
 
+  public incrementar(): void {
+    this.elValor++;
+  }
+  reducir(): void {
+    this.elValor--;
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
