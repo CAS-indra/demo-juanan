196324d5e4d238bed4aeeb809c2e04a20433c881
juanan83 <juanangelpueyo@outlook.com>
2021-09-22 01:56:53 +0200

    Agregado módulo contacto.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ const routes: Routes = [
     path: '',
     component: HomeComponent,
   },
+  { path: 'contacto', loadChildren: () => import('./contacto/contacto.module').then(m => m.ContactoModule) },
 ];
 
 @NgModule({
