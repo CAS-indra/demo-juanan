5fd590ff3847ca57265e5061a8c2387aab02d05b
juanan83 <juanangelpueyo@outlook.com>
2021-09-22 01:49:52 +0200

    Eliminado contenido para delegar a home.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,29 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  public proyectos = [
-    {
-      name: 'Aprender angular',
-      status: 1,
-    },
-    {
-      name: 'Tener un buen trabajo',
-      status: 0,
-    },
-  ];
-  public color = 'brown';
-
-  public elValor = 5;
-
-  public multiplo(cantidad: number): number {
-    return cantidad * this.elValor;
-  }
-
-  public incrementar(): void {
-    this.elValor++;
-  }
-  reducir(): void {
-    this.elValor--;
-  }
-}
+export class AppComponent {}
