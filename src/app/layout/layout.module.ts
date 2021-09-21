87946ea70f1f900ab2fede0c97dbe30bb88d669a
juanan83 <juanangelpueyo@outlook.com>
2021-09-22 01:06:34 +0200

    Creado componente flat desarrollador.

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CabeceraComponent } from './cabecera/cabecera.component';
 import { PieComponent } from './pie/pie.component';
+import { DesarrolladorComponent } from './desarrollador.component';
 
 @NgModule({
-  declarations: [CabeceraComponent, PieComponent],
+  declarations: [CabeceraComponent, PieComponent, DesarrolladorComponent],
   imports: [CommonModule],
   exports: [CabeceraComponent, PieComponent],
 })
