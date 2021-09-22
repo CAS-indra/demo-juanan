08b0532169fca8b154e434e529790dd9180071bf
juanan83 <juanangelpueyo@outlook.com>
2021-09-22 02:37:30 +0200

    Agregados los proyectos al servicio.

--- src/app/data/proyectos.service.ts
+++ src/app/data/proyectos.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProyectosService {
+  private proyectos = [
+    {
+      id: 'aprender-angular',
+      name: 'Aprender angular',
+      budget: 500,
+      status: 1,
+    },
+    {
+      id: 'tener-un-buen-trabajo',
+      name: 'Tener un buen trabajo',
+      budget: 50,
+      status: 0,
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
+
+  public getProyectos(): any[] {
+    return this.proyectos;
+  }
 }
